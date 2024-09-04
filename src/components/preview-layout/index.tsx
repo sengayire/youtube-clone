a629d0bb7c432899021780cc5d293dd8bfc152c4
sengayire <prince.s@cptim.com>
2024-09-04 04:46:57 +0100

    feat: search input

--- src/components/preview-layout/index.tsx
+++ src/components/preview-layout/index.tsx
@@ -1,5 +1,8 @@
 import React, { ReactNode } from 'react';
 import styles from './styles.module.css';
+import Image from 'next/image';
+import SearchInput from '../ui/search-input';
+import { AiFillBell } from 'react-icons/ai';
 
 type Props = {
   children: ReactNode;
@@ -9,7 +12,19 @@ export default function PreviewLayout({ children }: Props) {
   return (
     <div className={styles.layoutContainer}>
       <div className={styles.layoutHeader}>
-        <h3>Youtube clone</h3>
+        <Image
+          src='/happy-tube-logo-2.webp'
+          width={250}
+          height={50}
+          alt='Happy Tube'
+        />
+        <SearchInput />
+        <div className={styles.profile}>
+          <AiFillBell size={30} />
+          <div className={styles.placeholder}>
+            <img src='/placeholder.webp' className={styles.profilePicture} />
+          </div>
+        </div>
       </div>
       <div className={styles.listContainer}>
         <div className={styles.leftSideBar}></div>
